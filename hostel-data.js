f517eb47dd7a8071da58e62a43be6d74c53a2145
Josh <leong.jo@gmail.com>
2017-12-10 02:29:52 -0600

    hostel data start

---
+++ hostel-data.js
@@ -0,0 +1,19 @@
+const hostels = [{
+        id:
+        hostel_name: 'HI San Francisco Downtown Hostel'
+        location: {
+                full_address: '312 Mason Street Union Square San Francisco, California 94102'
+                city: 'San Francisco'
+                state: 'CA'
+                zipcode: '94102'
+        }
+        contact: {
+                email: 'SFDowntown@hiusa.org'
+                phone: '415-788-5604'
+        }
+        misc: {
+            photo_url: 'https://www.hiusa.org/images/uploads/hostel/CA-hi-san-francisco-downtown-gallery-09.jpg'
+            website_url: 'https://www.hiusa.org/hostels/california/san-francisco/downtown'
+
+        }
+]
