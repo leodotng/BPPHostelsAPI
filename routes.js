2af6dd1f4feb7c88eb35c3772b1e51ed4f98154c
Josh <leong.jo@gmail.com>
2017-12-11 10:16:13 -0600

    route fix

--- routes.js
+++ routes.js
@@ -1,9 +1,13 @@
-// var express = require('express');
-// var router = express.Router();
+var express = require('express');
+var router = express.Router();
+var queries = require('./queries');
 
-// /* GET users listing. */
-// router.get('/api', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
+/* GET users listing. */
+router.get('/api', function(req, res, next) {
+  queries.getHostels()
+  .then((hostelData) => {
+    res.json(hostelData)
+  })
+});
 
-// module.exports = router;
+module.exports = router;
