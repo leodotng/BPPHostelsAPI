1dbed07409a0d1cd12d0ad103c1dee5d31f67c66
Josh <leong.jo@gmail.com>
2017-12-10 20:48:19 -0600

    API data

--- hostel-data.js
+++ hostel-data.js
@@ -1,19 +1,28 @@
-const hostels = [{
-        id:
-        hostel_name: 'HI San Francisco Downtown Hostel'
-        location: {
-                full_address: '312 Mason Street Union Square San Francisco, California 94102'
-                city: 'San Francisco'
-                state: 'CA'
-                zipcode: '94102'
-        }
-        contact: {
-                email: 'SFDowntown@hiusa.org'
-                phone: '415-788-5604'
-        }
-        misc: {
-            photo_url: 'https://www.hiusa.org/images/uploads/hostel/CA-hi-san-francisco-downtown-gallery-09.jpg'
-            website_url: 'https://www.hiusa.org/hostels/california/san-francisco/downtown'
+const hostels =
 
+{
+        'continent': {
+                'north_america': {
+                        'USA': {
+                                'CA': {
+                                        'San Francisco': {
+                                                id: {
+                                                hostel_name: 'HI San Francisco Downtown Hostel'
+                                                location: {
+                                                full_address: '312 Mason Street Union Square San Francisco, California 94102',
+                                                zipcode: '94102'
+                                                }
+                                                contact: {
+                                                email: 'SFDowntown@hiusa.org',
+                                                phone: '415-788-5604'
+                                                }
+                                                misc: {
+                                                photo_url: 'https://www.hiusa.org/images/uploads/hostel/CA-hi-san-francisco-downtown-gallery-09.jpg',
+                                                website_url: 'https://www.hiusa.org/hostels/california/san-francisco/downtown'
+                                                }
+                                        }
+                                }
+                        }
+                }
         }
-]
+}
